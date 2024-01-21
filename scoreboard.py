5e401fe4f3d943376dbd8cd413025067be6b829c
Kyle Hansen <kyle@hansenlabs.com>
2024-01-21 14:23:02 -0600

    Added config and LCD backgrounds

--- scoreboard.py
+++ scoreboard.py
@@ -4,6 +4,8 @@ import pygame
 import pygame.font
 from pygame.locals import *
 
+from config import GAME_TIME_POSITION, HOME_SCORE_POSITION, VISITOR_SCORE_POSITION, PERIOD_NUMBER_POSITION, \
+    LCD_BACKGROUND_COLOR
 from game import Game
 
 pygame.init()
@@ -33,31 +35,53 @@ while True:
     DISPLAYSURF.blit(background, background.get_rect())
 
     # render the game time
-    game_time = time_font.render(game.get_time_readout(), False, (255, 0, 0), (0, 0, 0))
+    game_time = time_font.render("00:00", False, LCD_BACKGROUND_COLOR, (0, 0, 0))
 
     game_time_rect = game_time.get_rect()
-    game_time_rect.center = (397, 118)
+    game_time_rect.center = GAME_TIME_POSITION
+    DISPLAYSURF.blit(game_time, game_time_rect)
+
+    game_time = time_font.render(game.get_time_readout(), False, (255, 0, 0))
+
+    game_time_rect = game_time.get_rect()
+    game_time_rect.center = GAME_TIME_POSITION
     DISPLAYSURF.blit(game_time, game_time_rect)
 
     # render the home score
-    home_score_surf = count_font.render("12", False, (255, 0, 0), (0, 0, 0))
+    score_background_surf = count_font.render("00", False, LCD_BACKGROUND_COLOR, (0, 0, 0))
+
+    score_background_rect = score_background_surf.get_rect()
+    score_background_rect.center = HOME_SCORE_POSITION
+    DISPLAYSURF.blit(score_background_surf, score_background_rect)
+
+    home_score_surf = count_font.render("12", False, (255, 0, 0))
 
     home_score_rect = home_score_surf.get_rect()
-    home_score_rect.center = (130, 238)
+    home_score_rect.center = HOME_SCORE_POSITION
     DISPLAYSURF.blit(home_score_surf, home_score_rect)
 
     # render the visitor score
-    visitor_score_surf = count_font.render("09", False, (255, 0, 0), (0, 0, 0))
+    score_background_rect = score_background_surf.get_rect()
+    score_background_rect.center = VISITOR_SCORE_POSITION
+    DISPLAYSURF.blit(score_background_surf, score_background_rect)
+
+    visitor_score_surf = count_font.render("09", False, (255, 0, 0))
 
     visitor_score_rect = visitor_score_surf.get_rect()
-    visitor_score_rect.center = (655, 238)
+    visitor_score_rect.center = VISITOR_SCORE_POSITION
     DISPLAYSURF.blit(visitor_score_surf, visitor_score_rect)
 
     # render the period
-    period_surf = count_font.render("2", False, (255, 0, 0), (0, 0, 0))
+    period_surf = count_font.render("0", False, LCD_BACKGROUND_COLOR, (0, 0, 0))
 
     period_rect = period_surf.get_rect()
-    period_rect.center = (397, 355)
+    period_rect.center = PERIOD_NUMBER_POSITION
+    DISPLAYSURF.blit(period_surf, period_rect)
+
+    period_surf = count_font.render("2", False, (255, 0, 0))
+
+    period_rect = period_surf.get_rect()
+    period_rect.center = PERIOD_NUMBER_POSITION
     DISPLAYSURF.blit(period_surf, period_rect)
 
 

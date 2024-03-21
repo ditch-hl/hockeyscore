8dcd515151737542f13c7baed12a629b20e3a456
Kyle Hansen <kyle@hansenlabs.com>
2024-03-20 21:52:47 -0500

    Adding time adjustment.

--- game.py
+++ game.py
@@ -4,21 +4,21 @@ import pygame
 from gpiozero import Button
 
 import gifs
-from config import PERIOD_LENGTH_IN_MINUTES, DELAY_AFTER_GOAL_IN_SECONDS
+from config import PERIOD_LENGTH_IN_MINUTES, DELAY_AFTER_GOAL_IN_SECONDS, DEBOUNCE_TIME
 
 button = Button(4, hold_time=3)
 
 home_score_pin = Button(27, pull_up=False)
 visitor_score_pin = Button(17, pull_up=False)
 
-home_score_up_pin = Button(14, bounce_time=0.25)
-home_score_down_pin = Button(15, bounce_time=0.25)
+home_score_up_pin = Button(14, bounce_time=DEBOUNCE_TIME)
+home_score_down_pin = Button(15, bounce_time=DEBOUNCE_TIME)
 
-visitor_score_up_pin = Button(8, bounce_time=0.25)
-visitor_score_down_pin = Button(7, bounce_time=0.25)
+visitor_score_up_pin = Button(7, bounce_time=DEBOUNCE_TIME)
+visitor_score_down_pin = Button(8, bounce_time=DEBOUNCE_TIME)
 
-game_time_up_pin = Button(5, bounce_time=0.25)
-game_time_down_pin = Button(6, bounce_time=0.25)
+game_time_up_pin = Button(6, bounce_time=DEBOUNCE_TIME)
+game_time_down_pin = Button(5, bounce_time=DEBOUNCE_TIME)
 
 
 class GameState(Enum):

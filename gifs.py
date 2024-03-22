ccfc94d7de097b00cc35bbe5d3c4b26416417e2d
Kyle Hansen <kyle@hansenlabs.com>
2024-03-22 01:02:59 -0500

    Adding gif jpeg retention.

--- gifs.py
+++ gifs.py
@@ -39,8 +39,8 @@ def load_gifs_from_directory(dir) -> list[tuple[str, pygame.Surface]]:
     result: list[tuple[str, pygame.Surface | pygame.SurfaceType]] = []
     for filepath in os.listdir(dir):
         if filepath.lower().endswith(".gif"):
-            jpg_filepath = f'{filepath[:filepath.rindex(".")]}.jpg'
-            if not os.path.exists(os.path.join("./jpgs", jpg_filepath)):
+            jpg_filepath = os.path.join("./jpgs", f'{filepath[:filepath.rindex(".")]}.jpg')
+            if not os.path.exists(jpg_filepath):
                 print(f"Generating new gif spritesheet: {jpg_filepath}")
                 jpg_filepath, gif_surf = convert_gif_to_spritesheet(os.path.join(dir, filepath))
                 result.append((os.path.join("jpgs", jpg_filepath), gif_surf))

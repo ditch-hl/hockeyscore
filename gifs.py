0b6d000604e2f0f2deb92fe19f70304087b9f34c
Kyle Hansen <kyle@hansenlabs.com>
2024-03-22 00:53:50 -0500

    Adding gif jpeg retention.

--- gifs.py
+++ gifs.py
@@ -125,7 +125,9 @@ def convert_gif_to_spritesheet(gif_filepath: str) -> [str, pygame.Surface]:
             im.seek(frame)
             spritesheet.paste(im, (frame * im.size[0], 0))
 
-        outfile = open(f'./jpgs/{gif_filepath[:gif_filepath.rindex(".")]}.jpg', "r+b")
+        _, gif_tail = os.path.split(gif_filepath)
+
+        outfile = open(f'./jpgs/{gif_tail[:gif_tail.rindex(".")]}.jpg', "r+b")
         spritesheet = spritesheet.resize(
             size=(int(im.size[0] * scale_factor * im.n_frames), int(im.size[1] * scale_factor)))
         spritesheet.save(outfile)
